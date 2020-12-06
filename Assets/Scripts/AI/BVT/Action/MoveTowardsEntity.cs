02a97bd0f4a0090b2e696e92738032535f6533a1
0a0r <hymscott@gmail.com>
2020-12-06 23:20:21 +0800

    Add AI methods
    - Add Melee enemy to scene.
    - Add Move towards entity method to BVD.
    - Add Cube environment.

---
+++ Assets/Scripts/AI/BVT/Action/MoveTowardsEntity.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class MoveTowardsEntity : Action
+{
+    // Aiming entity
+    public Transform AimingEntity;
+    
+    // Moving Speed
+    public float MovingSpeed = 0;
+
+    private Rigidbody rb;
+
+    private Vector3 aimingDirection;
+
+    private float aimingAngle = 0;
+
+    public override void OnStart()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if(!AimingEntity)
+        {
+            AimingEntity = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        aimingDirection = AimingEntity.position - transform.position;
+        aimingAngle = Mathf.Atan2(aimingDirection.y, aimingDirection.x) * Mathf.Rad2Deg;
+        return TaskStatus.Running;
+    }
+
+    public override void OnFixedUpdate()
+    {
+        if (rb)
+        {
+            rb.position = (rb.position + aimingDirection.normalized * MovingSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
