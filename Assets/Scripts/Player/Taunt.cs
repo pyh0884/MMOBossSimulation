c76ad792ed1515905c0dd979e55b20ef988fca77
pyh0884 <pyh2084@hotmail.com>
2020-12-09 22:50:53 +0800

    Revert "Revert "Add melee enemy AI""
    This reverts commit 5657d2b6dc806287341d36de6858372da480c3a1.

---
+++ Assets/Scripts/Player/Taunt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taunt : MonoBehaviour
+{
+    public float duration = 5.0f;
+    private void OnEnable()
+    {
+        StartCoroutine("deactivateSkill");
+        FindObjectOfType<PlayerMovement>().Taunted(transform, 5.0f);
+    }
+
+    IEnumerator deactivateSkill()
+    {
+        yield return new WaitForSeconds(duration);
+        gameObject.SetActive(false);
+    }
+}
