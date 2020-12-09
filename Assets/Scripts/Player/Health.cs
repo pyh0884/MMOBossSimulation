cf5027145e079944d87f746d5b10c8dfebb320b5
pyh0884 <pyh2084@hotmail.com>
2020-12-09 21:40:13 +0800

    Remove Enemy Prefab
    - 敌人基类预制体更名
    - 血条归零后死亡

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -8,11 +8,25 @@ public class Health : MonoBehaviour
     public Slider healthBar;
     public float maxHealth = 100.0f;
     public float currentHealth = 100.0f;
+    private bool isDead;
     public void TakeDamege(float damage)
     {
+        if (isDead) return;
         currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Dead();
+        }
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
     }
+
+    public void Dead() 
+    {
+        isDead = true;
+        Destroy(gameObject);
+        //TODO: Add death animation/EFX
+    }
+
     void Update()
     {
         healthBar.value = currentHealth / maxHealth;
